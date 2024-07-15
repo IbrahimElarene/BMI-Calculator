df737cb382d3ebb1c637657dc8e70016f42cbd65
IbrahimElarene <146035952+IbrahimElarene@users.noreply.github.com>
2024-07-16 00:57:35 +0300

    Add files via upload

---
+++ lib/bmi-result-screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class BMIResultScreen extends StatelessWidget {
+  final int result;
+  final bool isMale;
+  final int age;
+  BMIResultScreen({
+    required this.result,
+    required this.age,
+    required this.isMale,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("BMI Result"),
+        leading: IconButton(
+            icon: Icon(Icons.keyboard_arrow_left),
+            onPressed: () {
+              Navigator.pop(context);
+            }),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Gender:${isMale ? 'Male' : 'Female'}",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "Result:$result",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "Age:$age",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
